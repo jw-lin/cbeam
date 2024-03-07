0332aed918a3eea27def401d7bcbced7d19c233b
Jonathan Lin <90280749+jw-lin@users.noreply.github.com>
2024-03-07 15:26:36 -0800

    new setup scripts

---
+++ jlsetup.py
@@ -0,0 +1,9 @@
+from juliacall import Pkg as jlPkg
+import cbeam,os
+path = os.path.dirname(cbeam.__file__)
+
+jlPkg.activate(path+"/FEval")
+jlPkg.add("PythonCall")
+jlPkg.add("StaticArrays")
+jlPkg.add("GrundmannMoeller")
+jlPkg.add("Cubature")
--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 from setuptools import setup
 from setuptools.command.install import install
 import os
-from juliacall import Pkg as jlPkg
-class PostInstallCommand(install):
-    """Post-installation for installation mode. Install julia dependencies."""
-    def run(self):
-        install.run(self)
-        os.chdir("./src/cbeam")
-        jlPkg.activate("FEval")
-        jlPkg.add("PythonCall")
-        jlPkg.add("StaticArrays")
-        jlPkg.add("GrundmannMoeller")
-        jlPkg.add("Cubature")
 
-setup(include_package_data=True,cmdclass={'install': PostInstallCommand,},)
+setup(include_package_data=True)
 

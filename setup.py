adae4517af37015bd4fe1a4d1751ce74adfbade4
Jonathan Lin <90280749+jw-lin@users.noreply.github.com>
2024-03-07 15:10:24 -0800

    adding julia dep install

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
 from setuptools import setup
-
+from setuptools.command.install import install
+import os
 setup(include_package_data=True)
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode. Install julia dependencies."""
+    def run(self):
+        install.run(self)
+        import cbeam
+        path = os.path.dirname(cbeam.__file__)
+        os.chdir(path)
+        from juliacall import Pkg as jlPkg
+        jlPkg.activate("FEval")
+        jlPkg.add("PythonCall")
+        jlPkg.add("StaticArrays")
+        jlPkg.add("GrundmannMoeller")
+        jlPkg.add("Cubature")

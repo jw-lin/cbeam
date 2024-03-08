2dbbd5ed419fc52b00d9cacd6f3289ab31bcbecb
Jonathan Lin <jon880@astro.ucla.edu>
2024-03-08 14:12:08 -0800

    fix error message?

--- src/cbeam/FEvalsetup.py
+++ src/cbeam/FEvalsetup.py
@@ -1,11 +1,14 @@
 from juliacall import Pkg as jlPkg
+from juliacall import Main as jl
 import cbeam,os
 
 def FEvalsetup():
-    path = os.path.dirname(cbeam.__file__)
-    jlPkg.develop(jlPkg.PackageSpec(path = path+"\FEval") )
-    #jlPkg.activate(path+"/FEval")
     jlPkg.add("PythonCall")
+    jl.seval("using PythonCall")
+    path = os.path.dirname(cbeam.__file__)
+    #jlPkg.develop(jlPkg.PackageSpec(path = path+"\FEval") )
+    jlPkg.activate(path+"/FEval")
     jlPkg.add("StaticArrays")
+    jlPkg.add("PythonCall")
     jlPkg.add("GrundmannMoeller")
     jlPkg.add("Cubature")

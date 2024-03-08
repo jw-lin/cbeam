efa973b295c8e10f722283bf1240949966ced261
Jonathan Lin <jon880@astro.ucla.edu>
2024-03-08 13:56:16 -0800

    switch to develop instead of activate

--- src/cbeam/FEvalsetup.py
+++ src/cbeam/FEvalsetup.py
@@ -3,7 +3,8 @@ import cbeam,os
 
 def FEvalsetup():
     path = os.path.dirname(cbeam.__file__)
-    jlPkg.activate(path+"/FEval")
+    jlPkg.develop(jlPkg.PackageSpec(path = path+"\FEval") )
+    #jlPkg.activate(path+"/FEval")
     jlPkg.add("PythonCall")
     jlPkg.add("StaticArrays")
     jlPkg.add("GrundmannMoeller")
